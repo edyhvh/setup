c412799a5e14f029281c8af5282dacc7f0078d57
Jhonny <edy602425@gmail.com>
2026-04-29 03:12:43 -0300

    terminal aliases

---
+++ shell/aliases.zsh
@@ -0,0 +1,14 @@
+# Personal aliases sourced from ~/.zshrc.
+
+# Git shortcuts
+alias ga="git add"
+alias gcm="git commit -S -m"
+alias gp="git push"
+alias gpl="git pull"
+alias gc="git checkout"
+alias gpc="gh pr checkout"
+
+# Environment helpers
+alias ve="source .venv/bin/activate"
+alias python=python3
+alias pip=pip3
